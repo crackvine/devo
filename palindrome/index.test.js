bf0722b783be92bf5f20d2f2103a7d90aeebe376
crackvine <crackvine@personal>
2021-02-13 09:39:31 +0100

    Refactor tests

--- palindrome/index.test.js
+++ palindrome/index.test.js
@@ -1,37 +1,22 @@
 const { reverseString1, isPalindrome } = require('./index');
 
-const sample1 = 'hello';
-const reversed1 = 'olleh';
-
-const sample2 = 'abcdefghijklmnopqrstuvwxyz';
-const reversed2 = 'zyxwvutsrqponmlkjihgfedcba';
-
-const sample3 = 'esta mañana he tomado 3 cafés';
-const reversed3 = 'séfac 3 odamot eh anañam atse';
-
-const sample4 = '12121';
-const reversed4 = '12121';
-
-const sample5 = 'Ññçv&$!k1¨Ç´{&$}[))(())]';
-const reversed5 = ']))(())[}$&{´Ç¨1k!$&vçñÑ';
-
-const sample6 = '909/070/909';
-const reversed6 = '909/070/909';
+const testCases = [
+  { original: 'Hello', reversed: 'olleH', isPalindrome: false },
+  { original: 'abcdefghijklmnopqrstuvwxyz', reversed: 'zyxwvutsrqponmlkjihgfedcba', isPalindrome: false },
+  { original: 'esta mañana he tomado 4 cafés', reversed: 'séfac 4 odamot eh anañam atse', isPalindrome: false },
+  { original: '12121', reversed: '12121', isPalindrome: true },
+  { original: 'Ññçv&$!k1¨Ç´{&$}[))(())]', reversed: ']))(())[}$&{´Ç¨1k!$&vçñÑ', isPalindrome: false },
+  { original: '909/070/909', reversed: '909/070/909', isPalindrome: true },
+];
 
 test('should give inverse of sample strings', () => {
-  expect(reverseString1(sample1)).toEqual(reversed1);
-  expect(reverseString1(sample2)).toEqual(reversed2);
-  expect(reverseString1(sample3)).toEqual(reversed3);
-  expect(reverseString1(sample4)).toEqual(reversed4);
-  expect(reverseString1(sample5)).toEqual(reversed5);
-  expect(reverseString1(sample6)).toEqual(reversed6);
+  testCases.forEach((testCase) => {
+    expect(reverseString1(testCase.original)).toEqual(testCase.reversed);
+  });
 });
 
 test('should correctly identify palindromes of sample set', () => {
-  expect(isPalindrome(sample1)).toBe(false);
-  expect(isPalindrome(sample2)).toBe(false);
-  expect(isPalindrome(sample3)).toBe(false);
-  expect(isPalindrome(sample4)).toBe(true);
-  expect(isPalindrome(sample5)).toBe(false);
-  expect(isPalindrome(sample6)).toBe(true);
+  testCases.forEach((testCase) => {
+    expect(isPalindrome(testCase.original)).toBe(testCase.isPalindrome);
+  });
 });
