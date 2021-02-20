7d9bf237c1b237a5d87c71390927dd3a76c22ae3
crackvine <crackvine@personal>
2021-02-20 10:29:51 +0100

    refine palindrome tests

--- palindrome/index.test.js
+++ palindrome/index.test.js
@@ -1,12 +1,12 @@
 const { reverseString1, isPalindrome } = require('./index');
 
 const testCases = [
-  { original: 'Hello', reversed: 'olleH', isPalindrome: false },
-  { original: 'abcdefghijklmnopqrstuvwxyz', reversed: 'zyxwvutsrqponmlkjihgfedcba', isPalindrome: false },
-  { original: 'esta mañana he tomado 4 cafés', reversed: 'séfac 4 odamot eh anañam atse', isPalindrome: false },
-  { original: '12121', reversed: '12121', isPalindrome: true },
-  { original: 'Ññçv&$!k1¨Ç´{&$}[))(())]', reversed: ']))(())[}$&{´Ç¨1k!$&vçñÑ', isPalindrome: false },
-  { original: '909/070/909', reversed: '909/070/909', isPalindrome: true },
+  { original: 'Hello', reversed: 'olleH' },
+  { original: 'abcdefghijklmnopqrstuvwxyz', reversed: 'zyxwvutsrqponmlkjihgfedcba' },
+  { original: 'esta mañana he tomado 4 cafés', reversed: 'séfac 4 odamot eh anañam atse' },
+  { original: '12121', reversed: '12121' },
+  { original: 'Ññçv&$!k1¨Ç´{&$}[))(())]', reversed: ']))(())[}$&{´Ç¨1k!$&vçñÑ' },
+  { original: '909/070/909', reversed: '909/070/909' },
 ];
 
 test('should give inverse of sample strings', () => {
@@ -17,6 +17,6 @@ test('should give inverse of sample strings', () => {
 
 test('should correctly identify palindromes of sample set', () => {
   testCases.forEach((testCase) => {
-    expect(isPalindrome(testCase.original)).toBe(testCase.isPalindrome);
+    expect(isPalindrome(testCase.original)).toBe(testCase.original === testCase.reversed);
   });
 });
