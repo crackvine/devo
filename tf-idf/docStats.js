8d6c5ed7cfe3b28d150cf1243828753b5e14196b
crackvine <crackvine@personal>
2021-02-21 07:15:24 +0100

    fix naming, add wordCount to docStats, update tests

--- tf-idf/docStats.js
+++ tf-idf/docStats.js
@@ -1,7 +1,17 @@
-const termFrequency = (term, doc) => {
+const termCount = (term, doc) => {
   // Assuming the case of the search term is not relevant
   const termRegex = new RegExp(`\\b${term}\\b`, 'gi');
   return doc.match(termRegex)?.length ?? 0;
 };
 
-module.exports = { termFrequency };
+const wordCount = (doc) => {
+  // Assuming that all words are preceded with whitespace of some type
+  const matches = doc.match(/[^\s]+/g);
+  return matches?.length ?? 0;
+};
+
+const termFrequency = (term, doc) => (
+  wordCount(doc) > 0 ? termCount(term, doc) / wordCount(doc) : 0
+);
+
+module.exports = { termCount, wordCount, termFrequency };
