3719839f9b255d8460c34299897a7718aea31d07
crackvine <crackvine@personal>
2021-02-28 20:59:24 +0100

    fix broken paths after moving files

--- tf-idf/test/docStats.test.js
+++ tf-idf/test/docStats.test.js
@@ -3,7 +3,7 @@ const {
   wordCount,
   termFrequency,
   inverseDocumentFrequency,
-} = require('./docStats.js');
+} = require('../src/docStats.js');
 
 const sampleDocOne = `Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
 
